8bd4dbd8bf96ec6b19ffb1174fd29edb70149566
Srinivasan <srinijune10@gmail.com>
2024-07-30 16:56:06 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// let arr = ['abc','elf']
+//  arr.length = 0
+//  console.log(arr)
+
+// function findMax(arr){
+//     let max = arr[0]
+// for (let i = 0; i < arr.length; i++){
+//     if(arr[i]>max){
+//         max = arr[i]
+//     }
+// }
+// console.log(max)
+// }
+// let arr = [9,3,5,1,8]
+// findMax(arr)
+
+// function findSum(arr){
+//     let sum = 0
+//     for(let i = 0; i < arr.length; i++){
+//         sum = sum + arr[i]
+//     }
+//     console.log(sum)
+// }
+// let arr = [9,3,5,1,8]
+// findSum(arr)
